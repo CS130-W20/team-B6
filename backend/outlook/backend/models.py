1beace4d9f6b71d38f18851384f72aa7d7b8873c
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-17 23:56:48 -0800

    Added GET api for posts (#30)
    * Added GET api for posts
    
    * Remove pycache files
    
    * Fix inconsistent db
    
    * Remove pycache files

--- backend/outlook/backend/models.py
+++ backend/outlook/backend/models.py
@@ -3,13 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Post(models.Model):
-    creation_date = models.DateTimeField()
+    creation_timestamp = models.DateTimeField()
     like_count = models.IntegerField(default=0)
+    news_article_url = models.URLField()
 
     def __str__(self):
         rep = ''
-        rep += 'Creation Date: ' + str(self.creation_date) + '\n'
+        rep += 'Id: ' + str(self.id) + '\n'
+        rep += 'Creation Timestamp: ' + str(self.creation_timestamp) + '\n'
         rep += 'Like Count: ' + str(self.like_count) + '\n'
+        rep += 'News Article Url: ' + str(self.news_article_url) + '\n'
         return rep
 
 class User(models.Model):
--- backend/outlook/backend/views.py
+++ backend/outlook/backend/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, HttpResponse
 from django.core import serializers
 
+from backend.models import Post
 from backend.models import User
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is a response.")
 
+def get_post(request, post_id):
+    search_results = Post.objects.filter(id=post_id)
+    if search_results:
+        data = serializers.serialize('json', search_results)
+        return HttpResponse(data, content_type="application/json")
+    else:
+        return HttpResponse("No post with given post id found.")
+      
 def get_user(request, user_id):
     search_results = User.objects.filter(id=user_id)
     if search_results:
