75758e9059936d7fd73c7745bce7945742c1e82b
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-17 19:35:12 -0800

    Added GET api for posts

--- backend/outlook/backend/urls.py
+++ backend/outlook/backend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='empty_index'),
+        path('posts/<int:post_id>', views.get_post, name='get_post'),
 ]
--- backend/outlook/backend/views.py
+++ backend/outlook/backend/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from django.core import serializers
+
+from backend.models import Post
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is a response.")
+
+def get_post(request, post_id):
+    search_results = Post.objects.filter(id=post_id)
+    if search_results:
+        data = serializers.serialize('json', search_results)
+        return HttpResponse(data, content_type="application/json")
+    else:
+        return HttpResponse("No post with given post id found.")
