a2a37c3ab2f2576df08aa6111ce60c6128990b2e
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-18 00:11:01 -0800

    Fixed migration dependency chain

--- backend/outlook/backend/migrations/0003_update_post_model.py
+++ backend/outlook/backend/migrations/0003_update_post_model.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('backend', '0001_initial'),
+        ('backend', '0002_user'),
     ]
 
     operations = [
