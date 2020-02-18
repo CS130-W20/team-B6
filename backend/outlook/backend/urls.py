4fc62d7ff58316e89364b5864df906b791dac906
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-17 23:46:34 -0800

    Added basic user model (#29)
    * Added basic user model
    
    * Remove pycache files
    
    * Add pycache files to gitignore

--- backend/outlook/backend/urls.py
+++ backend/outlook/backend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='empty_index'),
+        path('users/<int:user_id>', views.get_user, name="get_user"),
 ]
--- backend/outlook/backend/views.py
+++ backend/outlook/backend/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from django.core import serializers
+
+from backend.models import User
 
 # Create your views here.
 def index(request):
     return HttpResponse("This is a response.")
+
+def get_user(request, user_id):
+    search_results = User.objects.filter(id=user_id)
+    if search_results:
+        data = serializers.serialize('json', search_results)
+        return HttpResponse(data, content_type="application/json")
+    else:
+        return HttpResponse("No user with given user id found.")
