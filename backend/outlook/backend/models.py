3cdf917dd705812a19f8d00f24a64fc0ad69cf78
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-17 19:22:31 -0800

    Added basic user model

--- backend/outlook/backend/models.py
+++ backend/outlook/backend/models.py
@@ -11,3 +11,22 @@ class Post(models.Model):
         rep += 'Creation Date: ' + str(self.creation_date) + '\n'
         rep += 'Like Count: ' + str(self.like_count) + '\n'
         return rep
+
+class User(models.Model):
+    user_name = models.CharField(unique=True, max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email_address = models.EmailField(max_length=70)
+    description = models.CharField(max_length=140)
+    profile_picture_url = models.URLField()
+
+    def __str__(self):
+        rep = ''
+        rep += 'id: ' + str(self.id) + '\n'
+        rep += 'user_name: ' + str(self.user_name) + '\n'
+        rep += 'first_name: ' + str(self.first_name) + '\n'
+        rep += 'last_name: ' + str(self.last_name) + '\n'
+        rep += 'email_address: ' + str(self.email_address) + '\n'
+        rep += 'description: ' + str(self.description) + '\n'
+        rep += 'profile_picture_url: ' + str(self.profile_picture_url) + '\n'
+        return rep
