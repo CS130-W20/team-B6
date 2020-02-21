998c4161bb095ae8ba2bcbcac2cbbed79fda26ed
samarseth <samarseth@aol.com>
2020-02-21 12:26:11 -0800

    added news-feed testing dart file

---
+++ frontend/test/news_feed.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:outlook/feed-stories.dart';
+import 'package:outlook/main.dart';
+
+void main() {
+  testWidgets('News Feed Tester Start...', (WidgetTester tester) async {
+    // Start building the app
+    await tester.pumpWidget(Outlook());
+    expect(find.text('Outlook'), findsOneWidget);
+    // Verify the presence of FeedStories widget in the feed
+    expect(find.byWidget(FeedStories), findsOneWidget);
+    //Verfiy the presence of the following icon, which is present in the newsfeed
+    expect(find.byIcon(Icons.announcement), findsNothing);
+    //Verfiy the presence of the following icon, which is present in the newsfeed
+    expect(find.byIcon(Icons.arrow_upward), findsNothing);
+  });
+}
