56f7a70e5bc026ae29486a23d5f672992351465c
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-02-20 18:43:38 -0800

    Completed basic function to generate news feed, added new requests dependency

--- backend/outlook/backend/news.py
+++ backend/outlook/backend/news.py
@@ -4,6 +4,12 @@ import requests
 # on public sources, so it is not a security risk.
 API_KEY = "06b29a383ab7424fb93a9d7fa418db6f"
 
+def config_for_external_scripts():
+    import os, sys, django
+    sys.path.append('/Users/rishabhjain/Documents/team-B6/backend/outlook')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "outlook.settings")
+    django.setup()
+
 def retrieve_top_articles():
     params = {
         "apiKey": API_KEY,
@@ -17,12 +23,17 @@ def retrieve_top_articles():
     return data["articles"]
 
 def save_article_data(article):
+    from backend.models import Article
     # Check if article already exists in the DB.
     # Primary key should be the article URL.
 
     # If article does not already exist, save it now.
-    article["source_name"] = article["source"]["name"]
-    # save article data here
+    new_article = Article()
+    new_article.source_name = article["source"]["name"]
+    new_article.title = article["title"]
+    new_article.author = article["author"]
+    new_article.publish_date = article["publishedAt"]
+    new_article.save()
     return 1
 
 def generate_news_feed():
