ff813ca18afa26d8971a4490ec63192d11959759
pacbac <jclaytonc99@gmail.com>
2020-02-27 15:03:19 -0800

    Remove ngrok

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -37,7 +37,7 @@ class _OutlookState extends State<Outlook> with SingleTickerProviderStateMixin {
   /// Calls the backend for user specific user data like name, email, etc.
   /// and passes into the global UserState for the entire application to use.
   void fetchUser() async {
-    final userDataResponse = await http.get('http://5405fdcd.ngrok.io/users/1');
+    final userDataResponse = await http.get('BACKEND API URL HERE');
     if (userDataResponse.statusCode == 200) {
        UserState state = UserState.fromJson(jsonDecode(userDataResponse.body)[0]);
        setState(() {
