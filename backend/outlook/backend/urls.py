dc84b806c31a66bf3dfeb1b86d3c9c10da315028
Rishabh Jain <rishabh.jain1198@gmail.com>
2020-03-06 20:26:37 -0800

    Created endpoint to create and update user

--- backend/outlook/backend/urls.py
+++ backend/outlook/backend/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
         path('', views.index, name='empty_index'),
         path('posts/<int:post_id>', views.get_post, name='get_post'),
         path('users/<int:user_id>', views.get_user, name="get_user"),
+        path('users/put/<int:user_id>', views.put_user, name="put_user"),
 ]
--- backend/outlook/backend/views.py
+++ backend/outlook/backend/views.py
@@ -27,3 +27,25 @@ def get_user(request, user_id):
         return HttpResponse(data, content_type="application/json")
     else:
         return HttpResponse("No user with given user id found.")
+
+def put_user(request, user_id):
+    """PUT interface for updating User details.
+    The request body (json) will be checked for the parameters in
+    the field_names list.
+    """
+
+    field_names = [
+        "first_name",
+        "last_name",
+        "email_address",
+        "description",
+        # profile_picture TODO: Need to implement an image upload service.
+    ]
+    if request.method != 'PUT':
+        return HttpResponse("Need to use PUT method for this interface.")
+    target_user = get_user(None, user_id)
+    for field_name in field_names:
+        if field_name in request.body:
+            setattr(target_user, field_name, request.body[field_name])
+    target_user.save()
+    return HttpResponse("Update operation successful.")
