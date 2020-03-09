9f26251b4669f7d562f52bb090dd0263e60540ac
samarseth <samarseth@aol.com>
2020-03-08 20:23:14 -0700

    added different logo styles

--- frontend/lib/feed-stories.dart
+++ frontend/lib/feed-stories.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
+
+
+
 class FeedStories extends StatelessWidget {
+  static Container MyArticles(String url) {
+      return Container(
+                width: 70.0,
+                height: 200.0,
+                decoration: new BoxDecoration(
+                  border: Border.all(width: 3.0,color: Colors.black),
+                  shape: BoxShape.rectangle,
+                  image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(url),
+                  )
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              );
+    }
 
   final topText = Row(
     mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -20,27 +38,35 @@ class FeedStories extends StatelessWidget {
   final stories = Expanded (
     child: new Padding(
       padding: const EdgeInsets.only(top:8.0),
-      child: new ListView.builder(
+      child: new ListView(
         scrollDirection: Axis.horizontal,
-        itemCount: 5,
-        itemBuilder: (context, index)=> new Stack(
-            alignment: Alignment.bottomRight,
+        // itemCount: 2,
+        // itemBuilder: (context, index)=> new Stack(
+            // alignment: Alignment.bottomRight,
             children: <Widget>[
-              new Container(
-                width: 70.0,
-                height: 200.0,
-                decoration: new BoxDecoration(
-                  border: Border.all(width: 3.0,color: Colors.black),
-                  shape: BoxShape.rectangle,
-                  image: new DecorationImage(
-                      fit: BoxFit.fill,
-                      image: new NetworkImage("https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/082012/bbc_news.png?itok=wf2PsHXO")
-                  )
-                ),
-                margin: const EdgeInsets.symmetric(horizontal: 8.0),
-              )
+              MyArticles("https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/082012/bbc_news.png?itok=wf2PsHXO"),
+              MyArticles("https://cdn.cnn.com/cnn/.e1mo/img/4.0/logos/CNN_logo_400x400.png"),
+              MyArticles("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSzsscufXOmVBrgtovLWCQaIsNLA118UpUFIWIUmZuRyJxWhfud"),
+              MyArticles("https://apprecs.org/gp/images/app-icons/300/bd/com.toi.reader.activities.jpg"),
+              MyArticles("https://media.glassdoor.com/sqll/104032/techcrunch-squarelogo-1513591957398.png"),
+              MyArticles("https://lh3.googleusercontent.com/proxy/FBojPBku0rEr0nWFXSTqWnsNNu7LT_0WeSlfXl902ZJ5e9jwENRYy8-iC6vS9jhLDd3PPBR1GC5ZHvFeq7agEIdazaTuoyWPlGabZ4wIixGmU_1lvc7YDZTyCJdqjuEtA3qkOn9zZfrVU-KBoQk4noErh6MjHdCi7rKtiP6px1bDWGr8WFwwMnPFRUwzMpJMxu6amV_B7Z3X0DcjfF13lodZvLCVKOlKUv8iu6duWNtH"),
+              MyArticles("https://yt3.ggpht.com/a/AGF-l7-wuYtDksmLBwlT5PE9LdMlKt0X2762ynpXLg=s900-c-k-c0xffffffff-no-rj-mo"),
+              // new Container(
+              //   width: 70.0,
+              //   height: 200.0,
+              //   decoration: new BoxDecoration(
+              //     border: Border.all(width: 3.0,color: Colors.black),
+              //     shape: BoxShape.rectangle,
+              //     image: new DecorationImage(
+              //         fit: BoxFit.fill,
+              //         image: new NetworkImage(url_temp),
+              //     )
+              //   ),
+              //   margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              // )
             ],
-        ),
+        // ),
+        
       ),
     )
   );
