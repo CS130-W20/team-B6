07db671d670fcc229a1d6850e2fa5e67a1fa0d1c
Clayton Chu <jclaytonc99@gmail.com>
2020-03-08 21:54:31 -0700

    Add signout button and switch to login page as default (#83)

--- frontend/lib/profile/profile_page.dart
+++ frontend/lib/profile/profile_page.dart
@@ -3,15 +3,39 @@ import 'package:outlook/profile/cover.dart';
 import 'package:outlook/profile/edit_profile_page.dart';
 import 'package:outlook/profile/section-control/section_control.dart';
 import 'package:outlook/states/user_state.dart';
+import 'package:hive/hive.dart';
+import 'package:outlook/states/auth_state.dart';
+import 'package:outlook/main.dart';
 
 /// The profile page has some options in its app bar, like an icon that leads to editing the profile page.
 List<Widget> getProfileActions(BuildContext context) {
+
+  void choiceAction(String action) async {
+    switch(action) {
+      case 'signout': {
+        await Hive.box(AuthState.AUTH_BOX).clear();
+        await Hive.box(UserState.USER_BOX).clear();
+        Navigator.popUntil(context, ModalRoute.withName(Navigator.defaultRouteName));
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Outlook()));
+      }
+    }
+  }
+
   return <Widget>[
     IconButton(
       icon: Icon(Icons.mode_edit),
       onPressed: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfilePage()));
       },
+    ),
+    PopupMenuButton<String>(
+      onSelected: choiceAction,
+      itemBuilder: (BuildContext context) => [
+        PopupMenuItem<String>(
+          value: 'signout',
+          child: Text('Sign Out')
+        )
+      ],
     )
   ];
 }
